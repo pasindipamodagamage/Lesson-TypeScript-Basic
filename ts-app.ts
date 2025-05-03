16a26267b568900afb4d22bad4bb22b8d0ddb278
pasindipamodagamage <pasidingamagae@gmail.com>
2025-05-03 14:11:25 +0530

    add Day-2 Lesson

--- ts-app.ts
+++ ts-app.ts
@@ -58,3 +58,117 @@ function testEnum(){
     console.log(moveString);
 }
 testEnum();
+
+// 8
+function testTuple(){
+    let person:[string,number]=["Saman",25];
+
+    console.log(person);
+    console.log(person[0]);
+    console.log(person[1]);
+}
+testTuple();
+
+// 9
+// this like getter yawana value ekama return krnw
+function testGenerics<T>(Value:T):T{
+    return Value;
+}
+console.log(testGenerics<string>("hi there"));
+console.log(testGenerics<number>(23));
+
+//10
+// generics with interface
+function testGenericsWithInterface(){
+
+    interface ApiResponse<T>{
+        data:T;
+        statusCode:number;
+    }
+
+    let response: ApiResponse<string>={
+        data:"User Data Added",
+        statusCode:200
+    }
+    console.log(response);
+}
+testGenericsWithInterface();
+
+//
+//
+// 11
+// object oriented in js
+
+interface Animal{
+    run():void;
+}
+
+class Human implements Animal{
+    private noOfLegs: number;
+    private noOfHands:number;
+
+    public constructor(noOfLegs:number,noOfHands:number) {
+        this.noOfLegs=noOfLegs;
+        this.noOfHands=noOfHands;
+    }
+
+    public setNoOfLegs(noOfLegs:number){
+        this.noOfLegs=noOfLegs;
+    }
+
+    public getNoOfLegs():number{
+        return this.noOfLegs;
+    }
+
+    public setNoOfHands(noOfHands:number){
+        this.noOfHands=noOfHands;
+    }
+
+    public getNoOfHands():number{
+        return this.noOfHands;
+    }
+
+
+    run():void{
+        console.log("Human is running");
+    }
+}
+
+class Employee extends Human{
+
+    private empCode:string;
+    private empName:string;
+
+    public constructor(noOfLegs:number,noOfHands:number,empCode:string,empName:string) {
+        super(noOfLegs,noOfHands);
+        this.empCode=empCode;
+        this.empName=empName;
+    }
+
+    public setEmpCode(empCode:string){
+        this.empCode=empCode;
+    }
+
+    public setEmpName(empName:string){
+        this.empName=empName;
+    }
+
+    public getEmpCode(){
+        return this.empCode;
+    }
+
+    public getEmpName(){
+        return this.empName;
+    }
+
+    run():void{
+        console.log("Employee is running");
+    }
+}
+
+let employee =new Employee(2,2,"Emp100","saman");
+
+console.log(employee.getEmpName());
+console.log(employee.getEmpCode());
+// console.log(employee.run());
+employee.run();
